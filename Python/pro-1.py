8057b6975155df30ac44a888788c14471533a5fa
anashali01 <saiyedanashali@gmail.com>
2025-01-25 21:25:50 +0530

    python start

---
+++ Python/pro-1.py
@@ -0,0 +1,19 @@
+# Normal Message print
+print("Hello World")
+
+# If Statement
+if 5 > 2:
+    print("2 is big")
+    if 3 < 10:
+        print("3 is small")
+
+# Variable Declare
+x = 10
+y = 2
+print(x + y)
+
+x = str(3)
+y = int(3)
+z = float(4)
+
+print(x  , y  , z)
