c4f235d504402856d5474db5c7ea653624bddc60
anashali01 <saiyedanashali@gmail.com>
2025-02-19 18:38:25 +0530

    funda-4 complete

---
+++ Python/Fundamentals/funda-4.py
@@ -0,0 +1,38 @@
+# For loop
+# for x in range(1 , 11):
+#     print(x)
+
+# num = int(input("Enter :"))
+# for x in range(2 , num):  #we can also declare the staring value .
+#     print(x)
+
+# for x in range(1 , 10):
+#     if x % 2 == 0:
+#         continue
+#     else:
+#         print(x)
+
+# for x in range (10):
+#     print("Hello world")
+
+
+#While loop
+# name = input("Enter name :")
+
+# while name == "":
+#     print("Please enter name")
+#     name = input("Enter name :")
+# print(f"hello {name}")
+
+# num = int(input("Enter number between 0-10 :"))
+
+# while num<0 or num>10 :
+#     print("Invalid number")
+#     num = int(input("Enter number between 0-10 :"))
+# print(f"You've enter {num}")
+
+# food = input("Enter food item press Q for exit")
+# while not food == "Q":
+#     food = input("Enter food item press Q for exit")
+
+# print("bye")
